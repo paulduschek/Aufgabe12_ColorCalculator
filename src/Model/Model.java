03e2587bde466b77bd730bdf835018d2924f335e
Paul Duschek <Paul.Duschek@HTL-Wels.at>
2021-01-26 13:02:29 +0100

    created Enum ColorCode.java, implemented Enum ColorCode.java

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -6,4 +6,19 @@
 package Model;
 
 public class Model {
+
+    //Variable Definitions
+
+    private ModularCounter red = new ModularCounter(0,256);
+    private ModularCounter green = new ModularCounter(0,256);
+    private ModularCounter blue = new ModularCounter(0,256);
+
+    public Model(ModularCounter r, ModularCounter g, ModularCounter b)
+    {
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+    }
+
+
 }
